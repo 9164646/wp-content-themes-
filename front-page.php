f939a17b33fb803a8a4add8ffdd17e3771bd04d4
9164646 <456jokka@gmail.com>
2025-03-21 16:07:24 +0530

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <h1 class="text-center my-4" style="color: #FFD700; text-shadow: 2px 2px 10px #FFD700;">Hanumaan Traders - Lucky Draw</h1>
+
+    <!-- Add the rest of your homepage code here -->
+    <?php include 'home-content.php'; ?>
+</div>
+
+<?php get_footer(); ?>
